67918b45898de6aeb4d39651846bff2a1ae1d9b7
simonwrede-foreseeti <48316503+simonwrede-foreseeti@users.noreply.github.com>
2021-11-05 08:54:09 +0100

    Release 1.0.0rc2 (#20)

--- securicad/model/__init__.py
+++ securicad/model/__init__.py
@@ -14,7 +14,7 @@
 
 # pyright: reportUnusedImport=false
 
-__version__ = "1.0.0rc1"
+__version__ = "1.0.0rc2"
 
 from . import es_serializer as es_serializer
 from . import json_serializer as json_serializer
